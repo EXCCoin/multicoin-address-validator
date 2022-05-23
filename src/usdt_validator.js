d1073798124b78bfa0c05e960f4a5d07d02ca447
Łukasz Rząsa <lukasz.rzasa@pragmaticcoders.com>
2022-05-23 14:28:27 +0200

    update version 6

--- src/usdt_validator.js
+++ src/usdt_validator.js
@@ -4,24 +4,29 @@ var TronValidator = require('./tron_validator');
 
 function checkAllValidators(address, currency, networkType) {
     return (
-        BTCValidator.isValidAddress(address, currency, networkType)
-        || ETHValidator.isValidAddress(address, currency, networkType)
-        || TronValidator.isValidAddress(address, currency, networkType)
+        BTCValidator.isValidAddress(address, currency, {networkType})
+        || ETHValidator.isValidAddress(address, currency)
+        || TronValidator.isValidAddress(address, currency, {networkType})
     );
 }
 
 module.exports = {
     isValidAddress: function (address, currency, opts) {
         if (opts) {
-            return JSON.stringify(opts)
-            console['log'](opts);
-            alert(opts);
             if (opts.chainType === 'erc20' || opts.coinChainName === 'Ethereum') {
-                return ETHValidator.isValidAddress(address, currency, opts.networkType);
+                return ETHValidator.isValidAddress(address, currency);
             } else if (opts.chainType === 'omni' || opts.coinChainName === 'Bitcoin') {
-                return BTCValidator.isValidAddress(address, currency, opts.networkType);
+                return BTCValidator.isValidAddress(
+                    address,
+                    currency,
+                    { networkType: opts.networkType}
+                );
             } else if (opts.chainType === 'trc20' || opts.coinChainName === 'Tron') {
-                return TronValidator.isValidAddress(address, currency, opts.networkType)
+                return TronValidator.isValidAddress(
+                    address,
+                    currency,
+                    { networkType: opts.networkType}
+                );
             } else {
                 return checkAllValidators(address, currency, opts);
             }
