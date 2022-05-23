d25ac8d969dcda56e850fce03a1d047b4590d280
Łukasz Rząsa <lukasz.rzasa@pragmaticcoders.com>
2022-05-23 14:38:19 +0200

    update version 7

--- dist/wallet-address-validator.js
+++ dist/wallet-address-validator.js
@@ -12991,20 +12991,59 @@ module.exports = {
     isValidAddress: function (address, currency, opts) {
         if (opts) {
             if (opts.chainType === 'erc20' || opts.coinChainName === 'Ethereum') {
+                return {
+                    chainType: opts.chainType,
+                    cond: `opts.chainType === 'erc20' || opts.coinChainName === 'Ethereum'`,
+                    condResult: [opts.chainType === 'erc20', opts.coinChainName === 'Ethereum'],
+                    result: ETHValidator.isValidAddress(address, currency),
+                    params: [address,
+                        currency]
+                }
                 return ETHValidator.isValidAddress(address, currency);
             } else if (opts.chainType === 'omni' || opts.coinChainName === 'Bitcoin') {
+                return {
+                    chainType: opts.chainType,
+                    cond: `opts.chainType === 'omni' || opts.coinChainName === 'Bitcoin'`,
+                    condResult: [opts.chainType === 'omni', opts.coinChainName === 'Bitcoin'],
+                    result: BTCValidator.isValidAddress(
+                        address,
+                        currency,
+                        { networkType: opts.networkType}
+                    ),
+                    params: [address,
+                        currency,
+                        { networkType: opts.networkType}]
+                }
                 return BTCValidator.isValidAddress(
                     address,
                     currency,
                     { networkType: opts.networkType}
                 );
             } else if (opts.chainType === 'trc20' || opts.coinChainName === 'Tron') {
+                return {
+                    chainType: opts.chainType,
+                    cond: `opts.chainType === 'trc20' || opts.coinChainName === 'Tron'`,
+                    condResult: [opts.chainType === 'trc20', opts.coinChainName === 'Tron'],
+                    result: TronValidator.isValidAddress(
+                        address,
+                        currency,
+                        { networkType: opts.networkType}
+                    ),
+                    params: [address,
+                        currency,
+                        { networkType: opts.networkType}]
+                }
                 return TronValidator.isValidAddress(
                     address,
                     currency,
                     { networkType: opts.networkType}
                 );
             } else {
+                return {
+                    chainType: opts.chainType,
+                    cond: `else`,
+                    condResult: [opts.chainType === 'trc20', opts.coinChainName === 'Tron'],
+                }
                 return checkAllValidators(address, currency, opts);
             }
         }
